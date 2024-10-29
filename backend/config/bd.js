cb019efaeb24b3df347b01f10fd0cd43c5952951
Nicolas Zanella <166475036+NikoZane@users.noreply.github.com>
2024-10-29 19:06:46 -0300

    Update bd.js

--- backend/config/bd.js
+++ backend/config/bd.js
@@ -1,9 +1,19 @@
 import { Sequelize } from 'sequelize';
 
 // Configuración de la base de datos
-const bd = new Sequelize('ecommerce', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize({
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  dialectModule: pg,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 async function conectarBD() {
